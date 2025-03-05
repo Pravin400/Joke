4b8bda681478de8a2c0af03dbaf6d5f90fe6b44d
max125-c <pravintambe5671@gmail.com>
2025-03-05 15:36:54 +0530

    Resolved all issues in the JOKE 🧑‍💻💖... but...............

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const punchline = document.getElementById("punch");
 const setup = document.getElementById("setup");
+const type = document.getElementById("type");
 function getJoke() {
     // const Api = `https://official-joke-api.appspot.com/random_joke`;
     const Api = `https://v2.jokeapi.dev/joke/Programming,Spooky`;
     fetch(Api)
     .then((res) => res.json())
     .then((data) => {
+        setup.type = data.type;
         setup.innerHTML = data.setup;
         punchline.innerHTML = data.delivery;
         
