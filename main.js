5f95d66a26a901e25068c846d6ed5ea887f4da54
max125-c <pravintambe5671@gmail.com>
2025-03-05 17:43:13 +0530

    Jokes are tricky Jokes are tricky—stay alert and enjoy the ride! 😜💻

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ const punchline = document.getElementById("punch");
 const setup = document.getElementById("setup");
 const type = document.getElementById("type");
 function getJoke() {
-    // const Api = `https://official-joke-api.appspot.com/random_joke`;
-    const Api = `https://v2.jokeapi.dev/joke/Programming,Spooky`;
+    const Api = `https://official-joke-api.appspot.com/random_joke`;
+    // const Api = `https://v2.jokeapi.dev/joke/Programming,Spooky`;
     fetch(Api)
     .then((res) => res.json())
     .then((data) => {
         setup.type = data.type;
         setup.innerHTML = data.setup;
-        punchline.innerHTML = data.delivery;
+        punchline.innerHTML = data.punchline;
+        // punchline.innerHTML = data.delivery;
         
     })
 }
